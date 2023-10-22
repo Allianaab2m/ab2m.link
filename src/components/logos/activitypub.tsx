ed949a5744690d5ef94b94a13949723e0157aa22
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-10-23 00:29:43 +0900

    Delete Next js files

--- src/components/logos/activitypub.tsx
+++
@@ -1,7 +0,0 @@
-import { SVGProps } from "react"
-
-export function SkillIconsActivitypubDark(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 256 256" {...props}><path fill="currentColor" fillRule="evenodd" d="m137.15 52l86.841 63.279v25.237l-86.841 63.655v-25.236l69.533-50.849l-69.533-50.473V52Z" clipRule="evenodd"></path><path fill="currentColor" fillRule="evenodd" d="m137.15 102.849l34.617 25.237l-34.617 25.236v-50.473Z" clipRule="evenodd"></path><path fill="currentColor" fillRule="evenodd" d="M119.841 52L33 115.279v25.237l69.533-50.473v101.322l17.308 12.806V52Zm-34.617 76.086L50.31 153.322l34.617 25.236v-50.472h.298Z" clipRule="evenodd"></path></svg >
-  )
-}
