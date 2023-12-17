e2ad966bd1749a4140d219f65b4d85bd0ebb7938
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-12-17 15:49:13 +0900

    wip: rework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-  markdown: {
-    remarkRehype: {
-      footnoteBackLabel: "戻る",
-      footnoteLabel: "脚注",
-    },
-    shikiConfig: {
-      theme: "github-dark-dimmed",
-    },
-    remarkPlugins: [],
-  },
+  integrations: [tailwind()]
 });
