b73bf7dcf05cb255d84b542bde523c47b9c3d690
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-08-02 05:20:58 +0900

    feat: fuwari

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,142 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap'
+import svelte from '@astrojs/svelte'
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import swup from '@swup/astro'
+import Compress from 'astro-compress'
+import icon from 'astro-icon'
+import Color from 'colorjs.io'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeComponents from 'rehype-components'
+import rehypeKatex from 'rehype-katex'
+import rehypeSlug from 'rehype-slug'
+import remarkDirective from 'remark-directive'
+import remarkGithubAdmonitionsToDirectives from 'remark-github-admonitions-to-directives'
+import remarkMath from 'remark-math'
+import { AdmonitionComponent } from './src/plugins/rehype-component-admonition.mjs'
+import { GithubCardComponent } from './src/plugins/rehype-component-github-card.mjs'
+import { parseDirectiveNode } from './src/plugins/remark-directive-rehype.js'
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
+import stylus from 'stylus'
 
-import expressiveCode, { ExpressiveCodeTheme } from "astro-expressive-code";
-import fs from "node:fs"
-
-const jsoncString = fs.readFileSync(new URL(`./flexppuccin.jsonc`, import.meta.url), 'utf-8')
-const flexppuccin = ExpressiveCodeTheme.fromJSONString(jsoncString)
+const oklchToHex = str => {
+  const DEFAULT_HUE = 250
+  const regex = /~?\d+(\.\d+)?/g
+  const matches = str.string.match(regex)
+  const lch = [matches[0], matches[1], DEFAULT_HUE]
+  return new Color('oklch', lch).to('srgb').toString({
+    format: 'hex',
+  })
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), expressiveCode({
-    defaultProps: {
-      wrap: true,
-      frame: "code",
-    },
-    styleOverrides: {
-      codeFontSize: '1rem'
-    },
-    themes: [flexppuccin]
-  })],
+  site: 'https://ab2m.link',
+  base: '/',
+  trailingSlash: 'always',
+  integrations: [
+    tailwind(),
+    swup({
+      theme: false,
+      animationClass: 'transition-swup-',
+      containers: ['main'],
+      smoothScrolling: true,
+      cache: true,
+      preload: true,
+      accessibility: true,
+      updateHead: true,
+      updateBodyClass: true,
+      globalInstance: true,
+    }),
+    icon({
+      include: {
+        'material-symbols': ['*'],
+        'fa6-brands': ['*'],
+        'fa6-regular': ['*'],
+        'fa6-solid': ['*'],
+        'simple-icons': ['*'],
+      },
+    }),
+    svelte(),
+    sitemap(),
+    Compress({
+      CSS: false,
+      Image: false,
+      Action: {
+        Passed: async () => true,
+      },
+    }),
+  ],
   markdown: {
-    remarkRehype: {
-      footnoteLabel: "脚注"
+    remarkPlugins: [
+      remarkMath,
+      remarkReadingTime,
+      remarkGithubAdmonitionsToDirectives,
+      remarkDirective,
+      parseDirectiveNode,
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeSlug,
+      [
+        rehypeComponents,
+        {
+          components: {
+            github: GithubCardComponent,
+            note: (x, y) => AdmonitionComponent(x, y, 'note'),
+            tip: (x, y) => AdmonitionComponent(x, y, 'tip'),
+            important: (x, y) => AdmonitionComponent(x, y, 'important'),
+            caution: (x, y) => AdmonitionComponent(x, y, 'caution'),
+            warning: (x, y) => AdmonitionComponent(x, y, 'warning'),
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          properties: {
+            className: ['anchor'],
+          },
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              className: ['anchor-icon'],
+              'data-pagefind-ignore': true,
+            },
+            children: [
+              {
+                type: 'text',
+                value: '#',
+              },
+            ],
+          },
+        },
+      ],
+    ],
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        onwarn(warning, warn) {
+          if (
+            warning.message.includes('is dynamically imported by') &&
+            warning.message.includes('but also statically import ed by')
+          ) {
+            return
+          }
+        },
+      },
     },
-  }
-});
+    css: {
+      preprocessorOptions: {
+        stylus: {
+          define: {
+            oklchToHex: oklchToHex,
+          },
+        },
+      },
+    },
+  },
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,94 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
-const colors = {
-  base: {
-    black: "#100F0F",
-    950: "#1C1B1A",
-    900: "#282726",
-    850: "#343331",
-    800: "#403E3C",
-    700: "#575653",
-    600: "#6F6E69",
-    500: "#878580",
-    300: "#B7B5AC",
-    200: "#CECDC3",
-    150: "#DAD8CE",
-    100: "#E6E4D9",
-    50: "#F2F0E5",
-    paper: "#FFFCF0",
-  },
-  red: {
-    DEFAULT: "#AF3029",
-    light: "#D14D41",
-  },
-  orange: {
-    DEFAULT: "#BC5215",
-    light: "#DA702C",
-  },
-  yellow: {
-    DEFAULT: "#AD8301",
-    light: "#D0A215",
-  },
-  green: {
-    DEFAULT: "#66800B",
-    light: "#879A39",
-  },
-  cyan: {
-    DEFAULT: "#24837B",
-    light: "#3AA99F",
-  },
-  blue: {
-    DEFAULT: "#205EA6",
-    light: "#4385BE",
-  },
-  purple: {
-    DEFAULT: "#5E409D",
-    light: "#8B7EC8",
-  },
-  magenta: {
-    DEFAULT: "#A02F6F",
-    light: "#CE5D97",
-  },
-};
-
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}'],
+  darkMode: 'class', // allows toggling dark mode manually
   theme: {
-    colors,
-    fontFamily: {
-      'sans': ["IBM Plex Sans JP", ...defaultTheme.fontFamily.sans]
-    },
     extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            maxWidth: false,
-            // code: {
-            //   backgroundColor: theme('colors.base.900'),
-            //   borderRadius: theme('borderRadius.DEFAULT'),
-            //   paddingTop: theme('spacing.[0.5]'),
-            //   paddingBottom: theme('spacing.[0.5]'),
-            //   paddingLeft: theme('spacing.[1.5]'),
-            //   paddingRight: theme('spacing.[1.5]'),
-            //   fontWeight: 'normal',
-            // },
-            // 'code::before': {
-            //   content: 'none',
-            // },
-            // 'code::after': {
-            //   content: 'none',
-            // },
-          },
-        },
-        '2xl': {
-          css: {
-            fontSize: '2rem',
-          }
-        }
-      })
-    }
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [
-    require("@tailwindcss/typography")
-  ],
-};
+  plugins: [require('@tailwindcss/typography')],
+}
