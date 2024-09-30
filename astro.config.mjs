71810309eef8f431264f5b6d40b5309dd0424fda
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-09-30 12:19:14 +0900

    chore: add shiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,46 @@
-import sitemap from '@astrojs/sitemap'
-import svelte from '@astrojs/svelte'
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import swup from '@swup/astro'
-import Compress from 'astro-compress'
-import icon from 'astro-icon'
-import Color from 'colorjs.io'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeComponents from 'rehype-components'
-import remarkEmoji from 'remark-emoji'
-import rehypeKatex from 'rehype-katex'
-import rehypeSlug from 'rehype-slug'
-import remarkDirective from 'remark-directive'
-import remarkGithubAdmonitionsToDirectives from 'remark-github-admonitions-to-directives'
-import remarkMath from 'remark-math'
-import { AdmonitionComponent } from './src/plugins/rehype-component-admonition.mjs'
-import { GithubCardComponent } from './src/plugins/rehype-component-github-card.mjs'
-import { parseDirectiveNode } from './src/plugins/remark-directive-rehype.js'
-import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
-import { rawFonts } from './src/plugins/vite-raw-fonts.mjs'
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import swup from "@swup/astro";
+import Compress from "astro-compress";
+import icon from "astro-icon";
+import Color from "colorjs.io";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeComponents from "rehype-components";
+import remarkEmoji from "remark-emoji";
+import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
+import remarkDirective from "remark-directive";
+import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
+import remarkMath from "remark-math";
+import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.mjs";
+import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.mjs";
+import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import { rawFonts } from "./src/plugins/vite-raw-fonts.mjs";
 
-const oklchToHex = str => {
-  const DEFAULT_HUE = 250
-  const regex = /~?\d+(\.\d+)?/g
-  const matches = str.string.match(regex)
-  const lch = [matches[0], matches[1], DEFAULT_HUE]
-  return new Color('oklch', lch).to('srgb').toString({
-    format: 'hex',
-  })
-}
+const oklchToHex = (str) => {
+  const DEFAULT_HUE = 250;
+  const regex = /~?\d+(\.\d+)?/g;
+  const matches = str.string.match(regex);
+  const lch = [matches[0], matches[1], DEFAULT_HUE];
+  return new Color("oklch", lch).to("srgb").toString({
+    format: "hex",
+  });
+};
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ab2m.link',
-  base: '/',
-  trailingSlash: 'always',
+  site: "https://ab2m.link",
+  base: "/",
+  trailingSlash: "always",
   integrations: [
     tailwind(),
     swup({
       theme: false,
-      animationClass: 'transition-swup-',
-      containers: ['main'],
+      animationClass: "transition-swup-",
+      containers: ["main"],
       smoothScrolling: true,
       cache: true,
       preload: true,
@@ -51,11 +51,11 @@ export default defineConfig({
     }),
     icon({
       include: {
-        'material-symbols': ['*'],
-        'fa6-brands': ['*'],
-        'fa6-regular': ['*'],
-        'fa6-solid': ['*'],
-        'simple-icons': ['*'],
+        "material-symbols": ["*"],
+        "fa6-brands": ["*"],
+        "fa6-regular": ["*"],
+        "fa6-solid": ["*"],
+        "simple-icons": ["*"],
       },
     }),
     svelte(),
@@ -69,6 +69,9 @@ export default defineConfig({
     }),
   ],
   markdown: {
+    shikiConfig: {
+      langs: ["javascript", "gleam"],
+    },
     remarkPlugins: [
       remarkMath,
       remarkEmoji,
@@ -85,32 +88,32 @@ export default defineConfig({
         {
           components: {
             github: GithubCardComponent,
-            note: (x, y) => AdmonitionComponent(x, y, 'note'),
-            tip: (x, y) => AdmonitionComponent(x, y, 'tip'),
-            important: (x, y) => AdmonitionComponent(x, y, 'important'),
-            caution: (x, y) => AdmonitionComponent(x, y, 'caution'),
-            warning: (x, y) => AdmonitionComponent(x, y, 'warning'),
+            note: (x, y) => AdmonitionComponent(x, y, "note"),
+            tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+            important: (x, y) => AdmonitionComponent(x, y, "important"),
+            caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+            warning: (x, y) => AdmonitionComponent(x, y, "warning"),
           },
         },
       ],
       [
         rehypeAutolinkHeadings,
         {
-          behavior: 'append',
+          behavior: "append",
           properties: {
-            className: ['anchor'],
+            className: ["anchor"],
           },
           content: {
-            type: 'element',
-            tagName: 'span',
+            type: "element",
+            tagName: "span",
             properties: {
-              className: ['anchor-icon'],
-              'data-pagefind-ignore': true,
+              className: ["anchor-icon"],
+              "data-pagefind-ignore": true,
             },
             children: [
               {
-                type: 'text',
-                value: '#',
+                type: "text",
+                value: "#",
               },
             ],
           },
@@ -123,15 +126,15 @@ export default defineConfig({
       rollupOptions: {
         onwarn(warning, warn) {
           if (
-            warning.message.includes('is dynamically imported by') &&
-            warning.message.includes('but also statically import ed by')
+            warning.message.includes("is dynamically imported by") &&
+            warning.message.includes("but also statically import ed by")
           ) {
-            return
+            return;
           }
         },
       },
     },
-    plugins: [rawFonts(['.woff2', '.ttf', '.woff', '.otf'])],
+    plugins: [rawFonts([".woff2", ".ttf", ".woff", ".otf"])],
     css: {
       preprocessorOptions: {
         stylus: {
@@ -142,10 +145,10 @@ export default defineConfig({
       },
     },
     ssr: {
-      external: ['@resvg/resvg-js'],
+      external: ["@resvg/resvg-js"],
     },
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
+      exclude: ["@resvg/resvg-js"],
     },
   },
-})
+});
